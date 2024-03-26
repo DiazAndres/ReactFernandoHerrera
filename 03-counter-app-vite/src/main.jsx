9d27e27d5a541f3262917590021872e9fd3e04d3
Andres Diaz <heladio476@gmail.com>
2024-03-26 10:02:11 -0600

    Hook - useState

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import { HelloWorldApp } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
 import "./style.css";
+import { CounterApp } from "./CounterApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <HelloWorldApp /> */}
-    <FirstApp  />
+    <CounterApp value={2} />
   </React.StrictMode>
 );
