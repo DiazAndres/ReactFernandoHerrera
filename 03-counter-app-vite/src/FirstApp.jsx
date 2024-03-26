24e6579e1086c27e6f3f7bd0cdcfe606bf82f8fd
Andres Diaz <heladio476@gmail.com>
2024-03-25 22:54:16 -0600

    CounterAPP

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,30 @@
+// const newMessage = {
+//   message: "Hola Mundo",
+//   title: "Andres",
+// };
+
+// const getMessage = () => "Hola mundo";
+
+import PropTypes from "prop-types";
+
+export const FirstApp = ({ title, subTitle, name }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      {/* <code>{JSON.stringify(newMessage)}</code> */}
+      <p>{subTitle}</p>
+      <p>{name}</p>
+    </>
+  );
+};
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+};
+
+FirstApp.defaultProps = {
+  name: "Andrés Díaz",
+  subTitle: "No hay subtitulo",
+  title: "No hay titulo",
+};
