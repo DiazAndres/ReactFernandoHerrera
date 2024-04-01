485778e76ffb55b0cef27281d932cb73c35b16cc
Andres Diaz <heladio476@gmail.com>
2024-04-01 13:26:52 -0600

    Unique Categories - Gif-Expert-App

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    // console.log('first')
+    setCategories([newCategory, ...categories]);
+    // setCategories((cat) => [...cat, "Valorant"]);
+  };
   return (
     <>
+      {/* titulo */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+      <AddCategory
+        // setCategories={setCategories}
+        onNewCategory={onAddCategory}
+      />
+      {/* <button onClick={onAddCategory}>Agregar</button> */}
+
+      {/* Listado de Gif */}
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+      {/* Gif Item */}
     </>
   );
 };
