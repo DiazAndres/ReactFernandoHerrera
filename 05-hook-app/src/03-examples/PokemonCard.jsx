348a197175cda93f9984632d93566eb8bbd1c6fe
Andres Diaz <heladio476@gmail.com>
2024-06-09 08:44:18 -0600

    Ending - Profundizando Hooks Generales

--- 05-hook-app/src/03-examples/PokemonCard.jsx
+++ 05-hook-app/src/03-examples/PokemonCard.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { jsx } from "react/jsx-runtime";
 
 export const PokemonCard = ({ id, name, sprites = [] }) => {
+  const pRef = useRef();
+  const [boxSize, setBoxSize] = useState({ width: 0, height: 0 });
+  useLayoutEffect(() => {
+    const { height, width } = pRef.current.getBoundingClientRect();
+    setBoxSize({ width, height });
+  }, [sprites]);
+
   return (
-    <section style={{ height: 200 }}>
-      <h2 className="text-capitalize">
-        #{id} - {name}
-      </h2>
-      {/* Imágenes */}
-      <div>
-        {sprites.map((sprite) => (
-          <img key={sprite} src={sprite} alt={name} />
-        ))}
-      </div>
-    </section>
+    <>
+      <section style={{ height: 200 }}>
+        <h2 className="text-capitalize">
+          #{id} - {name}
+        </h2>
+        {/* Imágenes */}
+        <div>
+          {sprites.map((sprite) => (
+            <img ref={pRef} key={sprite} src={sprite} alt={name} />
+          ))}
+        </div>
+      </section>
+      <code>{JSON.stringify(boxSize)}</code>
+    </>
   );
 };
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -8,10 +8,14 @@ import "./index.css";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+// import { MemoHook } from "./06-memos/MemoHook";
+// import { CallbackHook } from "./06-memos/CallbackHook";
+import { Padre } from "./07-tarea-memo/Padre";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FocusScreen />
+    <Padre />
   </React.StrictMode>
 );
