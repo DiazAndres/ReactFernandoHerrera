992b54b408d2c7bfc5bfaaea727deb6552656cf5
Andres Diaz <heladio476@gmail.com>
2024-06-10 21:59:21 -0600

    useReducer - Resolución Crear componentes y emitir eventos

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
 import { todoReducer } from "./todoReducer";
+import { TodoList } from "./TodoList";
+import { TodoAdd } from "./TodoAdd";
 
 const initalState = [
   {
@@ -17,38 +19,25 @@ const initalState = [
 export const TodoApp = () => {
   const [todos, dispatchTodo] = useReducer(todoReducer, initalState);
 
+  const handleNewTodo = (todo) => {
+    console.log(todo);
+  };
+
   return (
     <>
-      <h1>TodoApp: {todos.length + 10} <small>pendientes: ({todos.length})</small></h1>
+      <h1>
+        TodoApp: {todos.length + 10} <small>pendientes: ({todos.length})</small>
+      </h1>
       <hr />
 
       <div className="row">
         <div className="col-7">
-          <ul className="list-group">
-            {todos.map((todo) => (
-              <li
-                className="list-group-item d-flex justify-content-between"
-                key={todo.id}
-              >
-                <span className="align-self-center">{todo.description}</span>
-                <button className="btn btn-danger">Borrar</button>
-              </li>
-            ))}
-          </ul>
+          <TodoList todos={todos} />
         </div>
         <div className="col-5">
           <h4>Agregar TODO</h4>
           <hr />
-          <form>
-            <input
-              type="text"
-              placeholder="¿Qué hay que hacer?"
-              className="form-control"
-            />
-            <button type="submit" className="btn btn-outline-primary mt-1">
-              Agregar
-            </button>
-          </form>
+          <TodoAdd onNewTodo={handleNewTodo}/>
         </div>
       </div>
     </>
